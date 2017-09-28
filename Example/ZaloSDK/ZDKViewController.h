8bcffc3a9c7aae0bbcefd41c27d9aa59de70b3d3
Liem Vo <liemvouy@gmail.com>
2017-09-28 16:45:10 +0700

    Initial commit

---
+++ Example/ZaloSDK/ZDKViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZDKViewController.h
+//  ZaloSDK
+//
+//  Created by acct<blob>=<NULL> on 09/28/2017.
+//  Copyright (c) 2017 acct<blob>=<NULL>. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZDKViewController : UIViewController
+
+@end
